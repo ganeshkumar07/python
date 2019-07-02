d45fd009f0f4fd88bf218ff5f938fda762d98ecc
ganeshkumar07 <52189262+ganeshkumar07@users.noreply.github.com>
2019-07-02 14:46:27 +0530

    Update leap.py

--- leap.py
+++ leap.py
@@ -3,10 +3,10 @@ if year%100==0:
       if year%400==0:
            print('yes')
         else:
-           print('No')
+           print('no')
 elif year%4==0:
       print(yes')
  else:
-      print('No')
+      print('no')
           
   
