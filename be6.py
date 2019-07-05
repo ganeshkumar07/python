e6137a25669ccffdbd717a32fba274f891aa5bbb
ganeshkumar07 <52189262+ganeshkumar07@users.noreply.github.com>
2019-07-05 14:51:55 +0530

    Create be6.py

---
+++ be6.py
@@ -0,0 +1,10 @@
+g,t=map(int,input().split())
+for i in range(g+1,t):
+    s=0
+    a=i
+    while(a>0):
+        c=a%10
+        s+=c**3
+        a//=10
+    if(i==s):
+            print(i,end=" ")
